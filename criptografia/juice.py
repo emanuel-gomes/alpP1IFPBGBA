359619fea21a3139289de721f42a89064837324a
Emanuel Gomes <emanuel.gfsdeveloper@gmail.com>
2021-04-24 22:52:13 -0300

    Update juice.py

--- criptografia/juice.py
+++ criptografia/juice.py
@@ -1,1 +1,15 @@
-print ("já já")
+crip = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p', 'q','r','s','t','u','v','x','w','y','z']
+arquivo = input('Digite o nome do arquivo aqui: ')
+troca = arquivo.replace("txt", "juice")
+quebra = list(troca)
+nome = quebra[0:-6]
+mutacao = ''
+cont = len(nome)
+
+for posicao in nome:
+    id = crip.index(posicao) + 1
+    mutacao+= str(id)
+    cont-=1
+    if(cont != 0):
+        mutacao+= '_'
+print(mutacao+'.juice')
