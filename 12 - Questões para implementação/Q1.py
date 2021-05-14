a433ad2979c779ea3322a77a06cfdfc43c2ae477
Emanuel Gomes <emanuel.gfsdeveloper@gmail.com>
2021-05-14 13:29:24 -0300

    Update Q1.py

--- 12 - Questões para implementação/Q1.py
+++ 12 - Questões para implementação/Q1.py
@@ -1,8 +1,3 @@
 def imc(peso, altura):
     return peso / altura**2
 
-peso = float(input("Digite seu peso aqui: "))
-altura = float(input("Digite seu altura aqui: "))
-
-imc = imc(peso, altura)
-print ("SEU IMC É: %.1f" %imc)
